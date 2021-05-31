d88acb81cc37a66a81d1bca31724d63c2baabfcf
Drish <drish.xd@gmail.com>
2021-05-31 13:13:28 +0530

    Aos Animation

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
 // tooltip
 
 $(function () {
-    $('[data-toggle="tooltip"]').tooltip();
-  });
+  $('[data-toggle="tooltip"]').tooltip();
+});
 
 //  hamburger Animation
 
-  $(document).ready(function(){
-    $('.hamburger').click(function(){
-      $(this).toggleClass('close');
-    });
+$(document).ready(function () {
+  $(".hamburger").click(function () {
+    $(this).toggleClass("close");
   });
+});
+
+// Parallax Effext
+
+var image = document.getElementsByClassName("main-img");
+new simpleParallax(image);
+
+var image = document.getElementsByClassName("picture");
+new simpleParallax(image, {
+  delay: 0.6,
+  transition: "cubic-bezier(0,0,0,1)",
+});
+
+// AOS
+
+AOS.init({
+  once: false,
+  duration: 1500,
+  anchorPlacement: 'center-bottom',
+  easing: "ease-in-out",
+  mirror: "true",
+});
